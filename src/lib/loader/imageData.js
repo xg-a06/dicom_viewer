d5784397fb20b7594b9034aa2d20509c03dc1b4b
xg-a06 <xg_jser@163.com>
2020-05-13 19:28:35 +0800

    暂时提交代码

--- src/lib/loader/imageData.js
+++ src/lib/loader/imageData.js
@@ -6,11 +6,12 @@ const getEncapsulatedImageFrame = () => {
 }
 
 const getUncompressedImageFrame = (dataSet) => {
-  const pixelDataElement =
-    dataSet.elements.x7fe00010 || dataSet.elements.x7fe00008;
+  const pixelDataElement = dataSet.elements.x7fe00010;
   const bitsAllocated = dataSet.uint16('x00280100');
   const rows = dataSet.uint16('x00280010');
   const columns = dataSet.uint16('x00280011');
+  // Samples per Pixel 
+  // 每一个像素的取样数，一般来说，CT，MR，DR等灰度图像都是1，而彩超等彩**图像都是3，分别表示R, G, B三个颜色通道。
   const samplesPerPixel = dataSet.uint16('x00280002');
 
   const pixelDataOffset = pixelDataElement.dataOffset;
--- src/sdk.js
+++ src/sdk.js
@@ -23,7 +23,7 @@ ajax({
 
     const pixelDataElement =
       dataSet.elements.x7fe00010 || dataSet.elements.x7fe00008;
-    // const bitsAllocated = dataSet.uint16('x00280100');
+    const bitsAllocated = dataSet.uint16('x00280100');
     const rows = dataSet.uint16('x00280010');
     const columns = dataSet.uint16('x00280011');
     const samplesPerPixel = dataSet.uint16('x00280002');
